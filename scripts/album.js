a64e740286945df684039ed2e2e0d5e250119ede
Zemog <jgomez630@aol.com>
2017-04-23 11:58:06 -0700

    added fixtures js

--- scripts/album.js
+++ scripts/album.js
@@ -1,49 +1,3 @@
-/*Create album Object*/
-var albumPicasso = {
-  title: 'The Colors',
-  artist: 'Pablo Picasso',
-  label: 'Cubism',
-  year: 1881,
-  albumArtUrl: 'assets/images/album_covers/01.png',
-  songs:[
-    {title: 'Blue', duration: '4:26'},
-    {title: 'Green', duration: '3:14'},
-    {title: 'Red', duration: '5:01'},
-    {title: 'Pink', duration: '3:21'},
-    {title: 'Magenta', duration: '3:15'},
-  ]
-};
-/*Create album Object*/
-var albumMarconi = {
-  title: 'The Telephone',
-  artist: 'Guglielmo Marconi',
-  label: 'EM',
-  year: '1909',
-  albumArtUrl: 'assets/images/album_covers/20.png',
-  songs:[
-    {title: 'Hello Operator?', duration: '1:01'},
-    {title: 'Ring,ring,ring?', duration: '5:01'},
-    {title: 'Fits in your pocket?', duration: '3:21'},
-    {title: 'Can you hear me?', duration: '3:14'},
-    {title: 'Wrong phone number?', duration: '2:15'},
-  ]
-};
-/*Create 3rd album Object*/
-var albumPaganni = {
-  title: 'The Trills',
-  artist: 'Marconi macaroni',
-  label: 'EMI',
-  year: '1809',
-  albumArtUrl: 'assets/images/album_covers/07.png',
-  songs:[
-    {title: 'Hello World?', duration: '1:01'},
-    {title: 'Ring,swing,ring?', duration: '5:01'},
-    {title: 'Fits like a glove?', duration: '3:21'},
-    {title: 'Can you hear me?', duration: '3:14'},
-    {title: 'Wrong phone number?', duration: '2:15'},
-  ]
-};
-
 /*Dynamically Generate Song Row Content*/
 var createSongRow = function(songNumber,songName,songLength){
   var template =
@@ -54,9 +8,9 @@ var createSongRow = function(songNumber,songName,songLength){
   +' </tr>'
   ;
   var $row = $(template);
-  $('.album-view-song-item td').each(function(){
-    console.log($(this));
-  })
+//  $('.album-view-song-item td').each(function(){
+//    console.log($(this));
+//  })
   var clickHandler = function(){
     var songNumber = $(this).attr('data-song-number');
     
---
+++ scripts/fixtures.js
@@ -0,0 +1,45 @@
+/*Create album Object*/
+var albumPicasso = {
+  title: 'The Colors',
+  artist: 'Pablo Picasso',
+  label: 'Cubism',
+  year: 1881,
+  albumArtUrl: 'assets/images/album_covers/01.png',
+  songs:[
+    {title: 'Blue', duration: '4:26'},
+    {title: 'Green', duration: '3:14'},
+    {title: 'Red', duration: '5:01'},
+    {title: 'Pink', duration: '3:21'},
+    {title: 'Magenta', duration: '3:15'},
+  ]
+};
+/*Create album Object*/
+var albumMarconi = {
+  title: 'The Telephone',
+  artist: 'Guglielmo Marconi',
+  label: 'EM',
+  year: '1909',
+  albumArtUrl: 'assets/images/album_covers/20.png',
+  songs:[
+    {title: 'Hello Operator?', duration: '1:01'},
+    {title: 'Ring,ring,ring?', duration: '5:01'},
+    {title: 'Fits in your pocket?', duration: '3:21'},
+    {title: 'Can you hear me?', duration: '3:14'},
+    {title: 'Wrong phone number?', duration: '2:15'},
+  ]
+};
+/*Create 3rd album Object*/
+var albumPaganni = {
+  title: 'The Trills',
+  artist: 'Paganni',
+  label: 'EMI',
+  year: '1809',
+  albumArtUrl: 'assets/images/album_covers/07.png',
+  songs:[
+    {title: 'Hello World?', duration: '1:01'},
+    {title: 'Ring,swing,ring?', duration: '5:01'},
+    {title: 'Fits like a glove?', duration: '3:21'},
+    {title: 'Can you hear me?', duration: '3:14'},
+    {title: 'Wrong phone number?', duration: '2:15'},
+  ]
+};
