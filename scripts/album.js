d1caf4442909f9ea4b90958b686cc4be6150e400
Zemog <jgomez630@aol.com>
2017-04-28 14:42:35 -0700

    chkpt-21-assignment-partial

--- scripts/album.js
+++ scripts/album.js
@@ -34,7 +34,7 @@ var createSongRow = function(songNumber,songName,songLength){
    '<tr class="album-view-song-item">'
   + '<td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
   +' <td class="song-item-title">' + songName + '</td>'
-  +' <td class="song-item-duration">' + songLength + '</td>'
+  +' <td class="song-item-duration">' + filterTimeCode(songLength) + '</td>'
   +' </tr>'
   ;
   var $row = $(template);
@@ -129,9 +129,13 @@ var setCurrentAlbum = function(album){
 var updateSeekBarWhileSongPlays = function(){
   if(currentSoundFile){
     currentSoundFile.bind('timeupdate',function(event){
-      var seekBarFillRatio = this.getTime() / this.getDuration()    
-      var $seekBar = $('.seek-control .seek-bar');
-      updateSeekPercentage($seekBar,seekBarFillRatio);
+      //var seekBarFillRatio = this.getTime() / this.getDuration()
+       var currentTime = this.getTime();
+       var songLength = this.getDuration();
+       var seekBarFillRatio = currentTime / songLength;
+       var $seekBar = $('.seek-control .seek-bar');
+       updateSeekPercentage($seekBar,seekBarFillRatio);
+      setCurrentTimeInPlayerBar(filterTimeCode(currentTime));      
     });
   }
 };
@@ -246,6 +250,7 @@ var updatePlayerBarSong = function(){
   $('.currently-playing .artist-name').text(currentAlbum.artist);
   $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
   $('.main-controls .play-pause').html(playerBarPauseButton);
+  setTotalTimeInPlayerBar(filterTimeCode(currentSongFromAlbum.length));
 };
 
 // Play & Pause button templates
@@ -276,7 +281,27 @@ var togglePlayFromPlayerBar = function(){
         currentSoundFile.pause();
   }    
 };
-
+var setCurrentTimeInPlayerBar = function(currentTime) {
+  var $currentTimeElement = $('.seek-control .current-time');
+  $currentTimeElement.text(currentTime); 
+ };
+var setTotalTimeInPlayerBar = function(totalTime) { 
+  var $totalTimeElement = $('.seek-control .total-time');
+  $totalTimeElement.text(totalTime); 
+ };
+var filterTimeCode = function(timeInSeconds) {
+  var seconds = Number.parseFloat(timeInSeconds);
+  var wholeSeconds = Math.floor(seconds);
+  var minutes = Math.floor(wholeSeconds / 60);
+  var remainingSeconds = wholeSeconds % 60;
+  var output = minutes + ':';
+     
+  if (remainingSeconds < 10) {
+      output += '0';   
+   }
+  output += remainingSeconds;
+  return output;
+ };
 $(document).ready(function(){
   setCurrentAlbum(albumPicasso);
   setupSeekBars();
